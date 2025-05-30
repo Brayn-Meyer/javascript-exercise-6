f8c1a46769bef76df54f5b89a990d8890e16c49c
Brayn-Meyer <coolmrepic9876@gmail.com>
2025-05-30 11:49:25 +0200

    Added Exercise 4

---
+++ Brayn Meyer - JavaScript - Exercise 4.js
@@ -0,0 +1,6 @@
+// TODO: Create a function called greet that takes a name parameter
+// TODO: Make the greet function return "Hello, [name]!"
+// TODO: Create a function called add that takes two parameters
+// TODO: Make the add function return the sum of both parameters
+// TODO: Call both functions and store their results in variables
+// TODO: Print the results to the console
